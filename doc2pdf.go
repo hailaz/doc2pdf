2b9af1d39a9be47b2e23d233e87ff5d8e0aa9815
hailaz <739476267@qq.com>
2025-02-17 10:28:13 +0800

    up

--- doc2pdf.go
+++ doc2pdf.go
@@ -80,7 +80,7 @@ func NewDocDownload(mainURL, outputDir string) *DocDownload {
 	}
 	log.Println("浏览器启动成功", u)
 
-	browser = rod.New().ControlURL(u).MustConnect().NoDefaultDevice()
+	browser = rod.New().ControlURL(u).MustConnect()
 	if isDebug {
 		browser.SlowMotion(time.Second)
 	}
